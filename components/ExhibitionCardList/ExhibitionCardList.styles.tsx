b1c7cde23a2dc8ed8edbee74a9641c62ab3368d9
juhee <77ooee@gmail.com>
2022-12-04 02:59:38 +0900

    feat: 반응형 스타일 적용

--- components/ExhibitionCardList/ExhibitionCardList.styles.tsx
+++ components/ExhibitionCardList/ExhibitionCardList.styles.tsx
@@ -1,10 +1,6 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  /** @TODO: remove */
-  width: 390px;
-  margin: auto;
-
   display: flex;
   flex-wrap: wrap;
   gap: 8px;
