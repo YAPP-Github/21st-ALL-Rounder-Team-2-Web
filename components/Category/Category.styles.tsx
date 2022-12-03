b0a8e7211cbd306efec701e73f768a6b77f88c32
juhee <77ooee@gmail.com>
2022-12-03 21:37:50 +0900

    refactor: 카테고리 컴포넌트 스타일 분리

---
+++ components/Category/Category.styles.tsx
@@ -0,0 +1,24 @@
+import styled, { css } from "styled-components";
+
+export const Wrapper = styled.div<{ $active: boolean }>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 8px 16px;
+  gap: 10px;
+
+  border-radius: 71px;
+  box-sizing: border-box;
+
+  ${({ $active }) =>
+    $active
+      ? css`
+          color: #fff;
+          background: #373737;
+        `
+      : css`
+          color: #333333;
+          background: rgba(178, 178, 178, 0.24);
+        `}
+`;
--- components/Category/Category.tsx
+++ components/Category/Category.tsx
@@ -1,32 +1,13 @@
 import React from "react";
-import styled, { css } from "styled-components";
+import * as S from "./Category.styles";
+// import { Wrapper } from "./Category.styles";
 
 interface Props {
   text?: string;
   active: boolean;
 }
 
-const Wrapper = styled.div<{ $active: boolean }>`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  padding: 8px 16px;
-  gap: 10px;
-
-  border-radius: 71px;
-  box-sizing: border-box;
-  
-  ${({ $active }) => $active ? css`
-    color: #fff;
-    background: #373737;
-  ` : css`
-    color: #333333;
-    background: rgba(178, 178, 178, 0.24);
-  `}
-`;
-
 export const Category = (props: Props) => {
   const { text, active } = props;
-  return <Wrapper $active={active}>{text}</Wrapper>;
+  return <S.Wrapper $active={active}>{text}</S.Wrapper>;
 };
