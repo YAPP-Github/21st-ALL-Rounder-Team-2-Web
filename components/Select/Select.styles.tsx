ba893ff848a4960f0fd70fa06fad52a9d2586f1d
juhee <77ooee@gmail.com>
2022-12-04 05:26:44 +0900

    feat: Select 컴포넌트 옵션 구현

--- components/Select/Select.styles.tsx
+++ components/Select/Select.styles.tsx
@@ -1,6 +1,10 @@
 import styled, { css } from "styled-components";
 
 export const Wrapper = styled.button`
+  position: relative;
+`;
+
+export const SelectWrapper = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -8,4 +12,38 @@ export const Wrapper = styled.button`
 
 export const Text = styled.div`
   margin-right: 9px;
-`
+`;
+
+export const OptionWrapper = styled.div`
+  position: absolute;
+  top: 40px;
+  right: 8px;
+  z-index: 1000;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 126px;
+  height: 98px;
+  padding: 4px 0;
+  background: #ffffff;
+  box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.12);
+  border-radius: 12px;
+`;
+
+export const Item = styled.div`
+  width: 94px;
+  padding: 12px 0;
+  margin: 0 16px;
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: ${(props) => (props.active ? 600 : 400)};
+  font-size: 14px;
+  line-height: 150%;
+  color: ${(props) => (props.active ? "#333333" : "#828282")};
+  border-bottom: 0.4px solid #bdbdbd;
+
+  &:nth-last-child(1) {
+    border-bottom: none;
+  }
+`;
--- pages/home.tsx
+++ pages/home.tsx
@@ -2,23 +2,89 @@ import React, { useCallback } from "react";
 import { CategoryList } from "../components/CategoryList/CategoryList";
 import { Divider } from "../components/Divider/Divider";
 import { Select } from "../components/Select/Select";
+import ExhibitionCardList from "../components/ExhibitionCardList/ExhibitionCardList";
 import { useSelectCategory } from "../hooks/useSelectCategory";
 import * as S from "../styles/home.styles";
 
 export default function Home() {
-  const categoryItems = [{ text: "전체 기록", active: false }, { text: "졸업전시", active: false }];
-  const { selectedIndex, selectCategoryByIndex: handleSelectCategory } = useSelectCategory()
+  const categoryItems = [
+    { text: "전체 기록", active: false },
+    { text: "졸업전시", active: false },
+  ];
+
+  const fixedExhibition = {
+    id: 123,
+    imageUrl: "https://picsum.photos/358",
+    title: "목조형가구학과 졸전",
+    date: "2022. 11. 08",
+  };
+
+  const exhibitionList = [
+    {
+      id: 1,
+      imageUrl: "https://picsum.photos/358",
+      title: "전시회명어쩌구저쩌구어쩌구저쩌구어쩌구저쩌구",
+      date: "2022. 11. 08",
+    },
+    {
+      id: 2,
+      imageUrl: "https://picsum.photos/358",
+      title: "전시회명어쩌구저쩌구",
+      date: "2022. 11. 08",
+    },
+    {
+      id: 3,
+      imageUrl: "https://picsum.photos/358",
+      title: "전시회명어쩌구저쩌구",
+      date: "2022. 11. 08",
+    },
+    {
+      id: 4,
+      imageUrl: "https://picsum.photos/358",
+      title: "전시회명어쩌구저쩌구",
+      date: "2022. 11. 08",
+    },
+    {
+      id: 5,
+      imageUrl: "https://picsum.photos/358",
+      title: "전시회명어쩌구저쩌구",
+      date: "2022. 11. 08",
+    },
+    {
+      id: 6,
+      imageUrl: "https://picsum.photos/358",
+      title: "전시회명어쩌구저쩌구",
+      date: "2022. 11. 08",
+    },
+  ];
+
+  const { selectedIndex, selectCategoryByIndex: handleSelectCategory } =
+    useSelectCategory();
+
+  const {
+    selectedIndex: selectedFilter,
+    selectCategoryByIndex: handleSelectFilter,
+  } = useSelectCategory();
 
   const handleRegisterCategory = useCallback(() => {}, []);
 
   return (
     <S.Wrapper>
-      <S.CategoryListStyled activeIndex={selectedIndex} items={categoryItems} onSelected={handleSelectCategory} onRegister={handleRegisterCategory} />
+      <S.CategoryListStyled
+        activeIndex={selectedIndex}
+        items={categoryItems}
+        onSelected={handleSelectCategory}
+        onRegister={handleRegisterCategory}
+      />
       <Divider />
       <S.Filter>
-        <Select />
+        <Select activeIndex={selectedFilter} onSelected={handleSelectFilter} />
       </S.Filter>
       <S.Content>
+        <ExhibitionCardList
+          fixedExhibition={fixedExhibition}
+          exhibitionList={exhibitionList}
+        />
       </S.Content>
     </S.Wrapper>
   );
