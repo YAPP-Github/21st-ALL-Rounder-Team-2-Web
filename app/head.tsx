d1a7074568090075060db439d493a5db4a0f0da7
juhee <77ooee@gmail.com>
2022-12-04 02:14:42 +0900

    feat: 폰트 적용

--- app/head.tsx
+++ app/head.tsx
@@ -2,6 +2,12 @@ export default async function Head() {
   return (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        rel="stylesheet"
+        as="style"
+        crossOrigin="anonymous"
+        href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard-dynamic-subset.css"
+      />
     </>
   );
 }
