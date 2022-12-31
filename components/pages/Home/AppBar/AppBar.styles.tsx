e6ba09cb1d40c15ae86dfacb0477195e334b5bda
bobeenlee <globaldev@naver.com>
2022-12-31 17:14:53 +0900

    feat: 앱바 컴포넌트 추가

--- components/pages/Home/AppBar/AppBar.styles.tsx
+++ components/pages/Home/AppBar/AppBar.styles.tsx
@@ -1,2 +1,23 @@
-import React from 'react'
+import styled from "styled-components";
 
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 18px 20px;
+  background-color: #161616;
+`;
+
+export const LogoGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const MenuGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 12px;
+`;
--- components/pages/Home/AppBar/AppBar.tsx
+++ components/pages/Home/AppBar/AppBar.tsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import * as S from "./AppBar.styles"
+import { ReactComponent as ARTIEIcon } from "../../../ui/Icon/ARTIE.svg"
+import { ReactComponent as BellIcon } from "../../../ui/Icon/bell.svg"
+import { ReactComponent as CalendarIcon } from "../../../ui/Icon/calendar.svg"
+import { ReactComponent as UserIcon } from "../../../ui/Icon/user.svg"
 
 interface Props {
   className?: string;
@@ -6,6 +11,15 @@ interface Props {
 
 export const HomeAppBar = (props: Props) => {
   return (
-    <div>HomeAppBar</div>
+    <S.Wrapper>
+      <S.LogoGroup>
+        <ARTIEIcon />
+      </S.LogoGroup>
+      <S.MenuGroup>
+        <BellIcon />
+        <CalendarIcon />
+        <UserIcon />
+      </S.MenuGroup>
+    </S.Wrapper>
   )
 }
