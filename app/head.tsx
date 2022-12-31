e0b6f41de205c44972010762d86ec6b1b29c6097
bobeenlee <globaldev@naver.com>
2022-12-31 20:23:07 +0900

    feat: 메타태그 수정

--- app/head.tsx
+++ app/head.tsx
@@ -2,17 +2,8 @@ export default function Head() {
   return (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      {/* <link
-        rel="stylesheet"
-        as="style"
-        crossOrigin="anonymous"
-        href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard-dynamic-subset.css"
-      /> */}
-      <meta
-        name="description"
-        content="A playground to explore new Next.js 13 app directory features such as nested layouts, instant loading states, streaming, and component level data fetching."
-      />
-      <title>Next.js App Directory Playground</title>
+      <meta name="description" content="InApp" />
+      <title>InApp</title>
     </>
   );
 }
