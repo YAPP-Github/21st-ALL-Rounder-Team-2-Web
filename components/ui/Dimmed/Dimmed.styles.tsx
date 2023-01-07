5365d1340161c12b54ffd3b6fd4da5515cafd55c
juhee <77ooee@gmail.com>
2023-01-07 11:02:20 +0900

    style: Dimmed 컴포넌트 위치 조정

--- components/ui/Dimmed/Dimmed.styles.tsx
+++ components/ui/Dimmed/Dimmed.styles.tsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
 export const Dimmed = styled.div`
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
   background: rgba(22, 22, 22, 0.6);
+  z-index: 1000;
 `;
