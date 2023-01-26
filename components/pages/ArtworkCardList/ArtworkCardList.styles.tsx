bfd9149110a1b7d1739a14db5054914465514253
juhee <77ooee@gmail.com>
2023-01-27 02:36:28 +0900

    style: center 정렬 제거

--- components/pages/ArtworkCardList/ArtworkCardList.styles.tsx
+++ components/pages/ArtworkCardList/ArtworkCardList.styles.tsx
@@ -3,11 +3,10 @@ import styled from "styled-components";
 export const Wrapper = styled.ul`
   width: 100%;
   display: flex;
-  justify-content: center;
   flex-wrap: wrap;
   gap: 31px 8px;
 
   & > li {
-    width: calc(50% - 8px);
+    width: calc(50% - 4px);
   }
 `;
