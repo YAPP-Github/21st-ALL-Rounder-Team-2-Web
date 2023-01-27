e6d0b65e24390b8579ffe61045eee77559d6b80f
kwonjuhee <62097867+kwonjuhee@users.noreply.github.com>
2023-01-27 12:59:24 +0900

    chore: 스토리북 절대경로 세팅 (#29)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,42 +1,47 @@
+const path = require("path");
+
 module.exports = {
-  "stories": [
-    '../**/*.stories.(tsx|mdx)',
-  ],
-  "addons": [
+  stories: ["../**/*.stories.(tsx|mdx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
   },
-  webpackFinal: async config => {
-   
-       // disable whatever is already set to load SVGs
-       config.module.rules
-       .filter(rule => rule.test.test('.svg'))
-       .forEach(rule => rule.exclude = /\.svg$/i);
- 
-     // add SVGR instead
-     config.module.rules.push({
-       test: /\.svg$/,
-       use: [
-         {
-           loader: '@svgr/webpack'
-         },
-         {
-           loader: 'file-loader',
-           options: {
-             name: 'static/media/[path][name].[ext]'
-           }
-         }
-       ],
-       type: 'javascript/auto',
-       issuer: {
-         and: [/\.(ts|tsx|js|jsx|md|mdx)$/]
-       }
-     });
+  webpackFinal: async (config) => {
+    // disable whatever is already set to load SVGs
+    config.module.rules
+      .filter((rule) => rule.test.test(".svg"))
+      .forEach((rule) => (rule.exclude = /\.svg$/i));
+
+    // add SVGR instead
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+        },
+        {
+          loader: "file-loader",
+          options: {
+            name: "static/media/[path][name].[ext]",
+          },
+        },
+      ],
+      type: "javascript/auto",
+      issuer: {
+        and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
+      },
+    });
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.join(__dirname, "src"),
+    };
+
     return config;
   },
-}
+};
