f229c2f23d3d05d5627169449b2ea04cb47bc138
bobeenlee <globaldev@naver.com>
2023-01-28 15:21:57 +0900

    feat(-): date picker 추가

--- components/ui/Picker/DatePicker/DatePicker.tsx
+++ components/ui/Picker/DatePicker/DatePicker.tsx
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { DatePicker as AntdDatePicker } from "antd-mobile";
+import { PickerDate } from "antd-mobile/es/components/date-picker/util";
+
 import * as S from "./DatePicker.styles";
 
 export interface Props {
   className?: string;
+  defaultValue?: PickerDate | null | undefined
+  open: boolean;
+  onClose: () => void;
+  onConfirm: (val: PickerDate) => void;
 }
 
 export const DatePicker = (props: Props) => {
-  const { className } = props;
-  return <S.Wrapper className={className}>hello world</S.Wrapper>;
-}
+  const { className, defaultValue, open, onClose, onConfirm } = props;
+
+  const labelRenderer = useCallback((type: string, data: number) => {
+    switch (type) {
+      case 'year':
+        return data + '년'
+      case 'month':
+        return data + '월'
+      default:
+        return data
+    }
+  }, [])
+
+  return (
+    <S.Wrapper className={className}>
+      <AntdDatePicker
+        defaultValue={defaultValue}
+        title=""
+        visible={open}
+        onClose={onClose}
+        onConfirm={onConfirm}
+        precision="month"
+        confirmText="확인"
+        cancelText="취소"
+        renderLabel={labelRenderer}
+      />
+    </S.Wrapper>
+  );
+};
 
 export default DatePicker;
