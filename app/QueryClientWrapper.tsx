94c88d5cc1ac200b5503cddc81e1c5fc39629b6b
juhee <77ooee@gmail.com>
2023-02-12 04:41:27 +0900

    refactor: lazy initialization으로 수정

--- app/QueryClientWrapper.tsx
+++ app/QueryClientWrapper.tsx
@@ -1,16 +1,10 @@
 "use client";
 
-import React from "react";
+import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+export default function QueryClientWrapper({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
 
-export default function QueryClientWrapper({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
