aaf69bd63bcc20a5956f4bcfe3d5deaced4428f2
juhee <77ooee@gmail.com>
2023-02-14 18:44:30 +0900

    feat: 전시 기록 없는 경우 처리

--- app/exhibition/[exhibitionId]/page.tsx
+++ app/exhibition/[exhibitionId]/page.tsx
@@ -9,19 +9,27 @@ import * as S from "./page.styles";
 
 export default function Page({ params }: { params: { exhibitionId: string } }) {
   const exhibitionId = Number(params.exhibitionId);
-  const {data: postInfo } = useGetPostInfo(exhibitionId);
-  const {data: artworkList} = useGetArtworkList(exhibitionId);
+  const { data: postInfo } = useGetPostInfo(exhibitionId);
+  const { data: artworkList } = useGetArtworkList(exhibitionId);
 
-  if (!postInfo) return notFound();;
+  if (!postInfo) return notFound();
 
   return (
     <S.Wrapper>
       <ExhibitionInfoHeader postInfo={postInfo} />
       <S.Content>
-        <ArtworkCardList
-          exhibitionId={exhibitionId}
-          artworkList={artworkList}
-        />
+        {artworkList ? (
+          <ArtworkCardList exhibitionId={exhibitionId} artworkList={artworkList} />
+        ) : (
+          <S.Empty>
+            <div>
+              아직 남겨진 전시 기록이 없어요!
+              <br />
+              하단 버튼을 통해 자유롭게 전시를 기록해보아요
+            </div>
+            <button type="button">전시기록 하러가기</button>
+          </S.Empty>
+        )}
       </S.Content>
     </S.Wrapper>
   );
