aae971e5ad12f24dd33b304112bc09138b37f9e0
juhee <77ooee@gmail.com>
2023-02-18 19:59:56 +0900

    fix: 스토리 인자 수정

--- components/pages/Home/CategoryList/CategoryList.stories.tsx
+++ components/pages/Home/CategoryList/CategoryList.stories.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { action } from "@storybook/addon-actions"
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 
-import { CategoryList } from './CategoryList';
+import { CategoryList } from "./CategoryList";
 
 export default {
-  title: 'Pages/Home/CategoryList',
+  title: "Pages/Home/CategoryList",
   component: CategoryList,
 } as ComponentMeta<typeof CategoryList>;
 
@@ -15,5 +15,8 @@ const Template: ComponentStory<typeof CategoryList> = (args) => <CategoryList {.
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  items: [{ text: "cat1", active: false }, { text: "cat2", active: false }], 
+  items: [
+    { name: "cat1", id: 0 },
+    { name: "cat2", id: 1 },
+  ],
 };
