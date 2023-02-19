9053ced7ad655cd6f503c13bc4b72c4b9415d893
juhee <77ooee@gmail.com>
2023-02-19 14:21:33 +0900

    feat: 마지막 작품 삭제 시도시 경고 모달 띄우기

--- components/pages/ArtworkDeleteAlertModal/ArtworkDeleteAlertModal.tsx
+++ components/pages/ArtworkDeleteAlertModal/ArtworkDeleteAlertModal.tsx
@@ -2,10 +2,11 @@ import Modal from "@/components/ui/Modal/Modal";
 import * as S from "./ArtworkDeleteAlertModal.styles";
 
 type Props = {
-  onClose?: () => void;
+  onClose: () => void;
+  onConfirm: () => void;
 };
 
-const ArtworkDeleteAlertModal = ({ onClose }: Props) => {
+const ArtworkDeleteAlertModal = ({ onClose, onConfirm }: Props) => {
   return (
     <Modal onClose={onClose}>
       <S.Wrapper>
@@ -15,8 +16,12 @@ const ArtworkDeleteAlertModal = ({ onClose }: Props) => {
         </S.Title>
         <S.Description>마지막 기록물을 삭제할 시 전시정보가 삭제됩니다</S.Description>
         <S.Options>
-          <button type="button">아니오</button>
-          <button type="button">예</button>
+          <button type="button" onClick={onClose}>
+            아니오
+          </button>
+          <button type="button" onClick={onConfirm}>
+            예
+          </button>
         </S.Options>
       </S.Wrapper>
     </Modal>
