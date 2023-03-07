564388e62591dc47e38a9f03a781b575932ba52b
juhee <77ooee@gmail.com>
2023-03-07 18:33:05 +0900

    refactor: 사용하지 않는 함수 제거

--- apis/exhibition.ts
+++ apis/exhibition.ts
@@ -1,13 +1,7 @@
-import { Exhibition } from "@/interfaces/exhibition";
 import { axiosInstance } from "@/libs/axios";
 import { ExhibitControllerApiFactory } from "@/__generate__/post";
 
 const factory = ExhibitControllerApiFactory(undefined, undefined, axiosInstance);
-
-export const getPostPage = (id: number, pagable: { size: number; page: number }) => {
-  const { size, page } = pagable;
-  return factory.getPostPage(id, size, page);
-};
 
 export const getAllPostPage = async ({
   size = 100,
@@ -26,53 +20,6 @@ export const getAllPostPage = async ({
   } catch (err) {
     return null;
   }
-};
-
-export const getExhibitionList = (): Promise<Exhibition[]> => {
-  return Promise.resolve([
-    {
-      id: 123,
-      imageUrl: "https://picsum.photos/358",
-      title: "목조형가구학과 졸전",
-      date: "2022. 11. 08",
-    },
-    {
-      id: 1,
-      imageUrl: "https://picsum.photos/358",
-      title: "전시회명어쩌구저쩌구어쩌구저쩌구어쩌구저쩌구",
-      date: "2022. 11. 08",
-    },
-    {
-      id: 2,
-      imageUrl: "https://picsum.photos/358",
-      title: "전시회명어쩌구저쩌구",
-      date: "2022. 11. 08",
-    },
-    {
-      id: 3,
-      imageUrl: "https://picsum.photos/358",
-      title: "전시회명어쩌구저쩌구",
-      date: "2022. 11. 08",
-    },
-    {
-      id: 4,
-      imageUrl: "https://picsum.photos/358",
-      title: "전시회명어쩌구저쩌구",
-      date: "2022. 11. 08",
-    },
-    {
-      id: 5,
-      imageUrl: "https://picsum.photos/358",
-      title: "전시회명어쩌구저쩌구",
-      date: "2022. 11. 08",
-    },
-    {
-      id: 6,
-      imageUrl: "https://picsum.photos/358",
-      title: "전시회명어쩌구저쩌구",
-      date: "2022. 11. 08",
-    },
-  ]);
 };
 
 export const togglePinById = (id: string) => {
