b0953186e80c934b21fc06f507093afba78ea223
juhee <77ooee@gmail.com>
2023-03-08 01:10:09 +0900

    fix: typo 수정

--- utils/datetime.ts
+++ utils/datetime.ts
@@ -1,5 +1,5 @@
 export const toYYYYMMDD = (date: Date = new Date()) => {
-  return `${date.getFullYear()}${date.getMonth() + 1}${date.getDate()}}`;
+  return `${date.getFullYear()}${date.getMonth() + 1}${date.getDate()}`;
 };
 
 export const getDateByYearAndMonth = (year: number, month: number) => {
@@ -7,8 +7,8 @@ export const getDateByYearAndMonth = (year: number, month: number) => {
   val.setFullYear(year);
   val.setMonth(month);
   return val;
-}
+};
 
 export const today = () => {
-  return new Date()
-}
+  return new Date();
+};
