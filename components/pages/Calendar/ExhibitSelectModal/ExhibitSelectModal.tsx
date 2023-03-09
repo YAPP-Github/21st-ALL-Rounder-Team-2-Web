0e6065382e0cc207526ac83ee3fb435ed3fb9702
juhee <77ooee@gmail.com>
2023-03-09 23:27:39 +0900

    feat: 날짜별 전시 목록 조회 api 연동

--- components/pages/Calendar/ExhibitSelectModal/ExhibitSelectModal.tsx
+++ components/pages/Calendar/ExhibitSelectModal/ExhibitSelectModal.tsx
@@ -2,28 +2,30 @@ import Modal from "@/components/ui/Modal/Modal";
 import { Divider } from "@/components/ui/Divider/Divider";
 import Portal from "@/components/ui/Portal/Portal";
 import Dimmed from "@/components/ui/Dimmed/Dimmed";
-import { ExhibitByDateResponseDto } from "@/__generate__/post";
+import { useGetExhibitsByDate } from "@/hooks/calendar";
+import { toYYYYMMDD } from "@/utils/datetime";
 import * as S from "./ExhibitSelectModal.styles";
 
 type Props = {
-  selectedDate: String;
-  exhibitList: Array<ExhibitByDateResponseDto>;
+  selectedDate: Date;
   onClose: () => void;
 };
 
-export const ExhibitSelectModal = ({ selectedDate, exhibitList, onClose }: Props) => {
+export const ExhibitSelectModal = ({ selectedDate, onClose }: Props) => {
+  const { data: exhibitsByDate } = useGetExhibitsByDate(selectedDate);
+
   return (
     <Portal>
       <Dimmed onClick={onClose} />
       <Modal headline="전시를 선택해주세요" onClose={onClose}>
         <S.SelectList>
-          {exhibitList.map(({ postName, postId }, index) => (
+          {exhibitsByDate?.map(({ postName, postId }, index) => (
             <>
               <S.SelectItem key={postId}>
                 <S.Name>{postName}</S.Name>
-                <S.Date>{selectedDate}</S.Date>
+                <S.Date>{toYYYYMMDD(selectedDate).replaceAll("-", ".")}</S.Date>
               </S.SelectItem>
-              {index !== exhibitList.length - 1 && <Divider />}
+              {index !== exhibitsByDate?.length - 1 && <Divider />}
             </>
           ))}
         </S.SelectList>
