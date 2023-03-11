cf96771f117f0989089892bc61b7ea5597cdb20d
juhee <77ooee@gmail.com>
2023-03-11 11:41:37 +0900

    chore: Storybook staticDir 설정

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,23 +2,18 @@ const path = require("path");
 
 module.exports = {
   stories: ["../**/*.stories.(tsx|mdx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
   },
+  staticDirs: ["../public"],
   // features: {
   //   babelModeV7: true,
   // },
   webpackFinal: async (config) => {
     // disable whatever is already set to load SVGs
-    config.module.rules
-      .filter((rule) => rule.test.test(".svg"))
-      .forEach((rule) => (rule.exclude = /\.svg$/i));
+    config.module.rules.filter((rule) => rule.test.test(".svg")).forEach((rule) => (rule.exclude = /\.svg$/i));
 
     // add SVGR instead
     config.module.rules.push({
