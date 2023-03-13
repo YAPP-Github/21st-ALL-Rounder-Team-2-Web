efa7707d98d1f513a108ed9454a4199b26a1f783
juhee <77ooee@gmail.com>
2023-03-13 17:56:51 +0900

    feat: artwork mocking api 핸들러 작성

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,4 +1,5 @@
+import { handlers as ArtworkHandlers } from "./handlers/artwork";
 import { handlers as CategoryHandler } from "./handlers/category";
 import { handlers as ExhibitHandlers } from "./handlers/exhibition";
 
-export const handlers = [...CategoryHandler, ...ExhibitHandlers];
+export const handlers = [...ArtworkHandlers, ...CategoryHandler, ...ExhibitHandlers];
