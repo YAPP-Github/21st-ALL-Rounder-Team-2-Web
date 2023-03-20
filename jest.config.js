3153279cb079f9110c933d584eeba8f89b74c36d
juhee <77ooee@gmail.com>
2023-03-20 17:12:40 +0900

    chore: jest에 mock svg 설정

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,27 @@
-const nextJest = require('next/jest')
+const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-})
+  dir: "./",
+});
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleNameMapper: {},
-  testEnvironment: 'jest-environment-jsdom',
-}
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: { "^@/(.*)$": "<rootDir>/$1" },
+  testEnvironment: "jest-environment-jsdom",
+};
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)
+const jestConfig = async () => {
+  // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+  const nextJestConfig = await createJestConfig(customJestConfig)();
+  return {
+    ...nextJestConfig,
+    moduleNameMapper: {
+      "^.+\\.svg$": "<rootDir>/mocks/svg.js",
+      ...nextJestConfig.moduleNameMapper,
+    },
+  };
+};
+
+module.exports = jestConfig;
