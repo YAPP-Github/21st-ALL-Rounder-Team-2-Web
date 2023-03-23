43405538ff1bce9240aca2d3353e65c961562779
juhee <77ooee@gmail.com>
2023-03-23 18:31:41 +0900

    chore: test용 queryclient 생성

---
+++ __test__/test-utils.tsx
@@ -0,0 +1,21 @@
+import { render } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const createTestQueryClient = () =>
+  new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+
+export function renderWithClient(ui: React.ReactElement) {
+  const testQueryClient = createTestQueryClient();
+  const { rerender, ...result } = render(<QueryClientProvider client={testQueryClient}>{ui}</QueryClientProvider>);
+  return {
+    ...result,
+    rerender: (rerenderUi: React.ReactElement) =>
+      rerender(<QueryClientProvider client={testQueryClient}>{rerenderUi}</QueryClientProvider>),
+  };
+}
