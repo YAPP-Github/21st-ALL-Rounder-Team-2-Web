693870ddedea8da2e5b7ad42751173222183e063
juhee <77ooee@gmail.com>
2023-03-24 22:20:44 +0900

    refactor: data fetching 로직 상위 컴포넌트로 이동

--- app/exhibition/[exhibitionId]/page.tsx
+++ app/exhibition/[exhibitionId]/page.tsx
@@ -2,17 +2,21 @@
 
 import ArtworkCardList from "@/components/pages/ArtworkCardList/ArtworkCardList";
 import ExhibitionInfoHeader from "@/components/pages/ExhibitionInfoHeader/ExhibitionInfoHeader";
+import { useGetPostInfo } from "@/hooks/exhibition";
 import * as S from "./page.styles";
 
 export default function Page({ params }: { params: { exhibitionId: string } }) {
   const exhibitionId = Number(params.exhibitionId);
+  const { data: postInfo } = useGetPostInfo(exhibitionId);
 
   return (
-    <S.Wrapper>
-      <ExhibitionInfoHeader exhibitionId={exhibitionId} />
-      <S.Content>
-        <ArtworkCardList exhibitionId={exhibitionId} />
-      </S.Content>
-    </S.Wrapper>
+    <>
+      <S.Wrapper>
+        <ExhibitionInfoHeader postInfo={postInfo} />
+        <S.Content>
+          <ArtworkCardList exhibitionId={exhibitionId} />
+        </S.Content>
+      </S.Wrapper>
+    </>
   );
 }
