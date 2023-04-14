a81bd4f1734c9cf79f64a6b846908c423ca6cf5c
juhee <77ooee@gmail.com>
2023-04-14 16:31:39 +0900

    fix: props type 할당 오류 수정

--- components/pages/Home/Category/Category.tsx
+++ components/pages/Home/Category/Category.tsx
@@ -1,13 +1,16 @@
-import React from "react";
 import * as S from "./Category.styles";
 
 interface Props {
   text?: string;
   active: boolean;
-  onClick: () => void;
+  onClick: (e: React.MouseEvent) => void;
 }
 
 export const Category = (props: Props) => {
   const { text, active, onClick } = props;
-  return <S.Wrapper $active={active} onClick={onClick}>{text}</S.Wrapper>;
+  return (
+    <S.Wrapper $active={active} onClick={onClick}>
+      {text}
+    </S.Wrapper>
+  );
 };
