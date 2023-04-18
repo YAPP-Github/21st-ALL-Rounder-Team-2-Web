6fd5fbf6eeb936e24cc1135744a22a895bdda252
juhee <77ooee@gmail.com>
2023-04-18 22:25:10 +0900

    fix: axios header 설정 오류 수정

--- libs/axios.ts
+++ libs/axios.ts
@@ -1,18 +1,17 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios from "axios";
 import { isBrowser } from "@/utils/browser";
 
 export const axiosInstance = axios.create({
   withCredentials: true,
 });
 
-axiosInstance.interceptors.request.use((config: AxiosRequestConfig) => {
+axiosInstance.interceptors.request.use((config) => {
   if (isBrowser()) {
     const accessToken = document.cookie
       .split("; ")
       .find((e) => e.startsWith("accessToken="))
       ?.split("=")[1];
-    config.headers = { ...config.headers, Authorization: accessToken };
-  } else {
+    config.headers.setAuthorization(`Bearer ${accessToken}`);
   }
   return config;
 });
