14a966a979a43aeed0148db909f9b34eec578f04
juhee <77ooee@gmail.com>
2023-04-18 18:38:32 +0900

    feat: 토큰 테스트 페이지

---
+++ app/test-token/client.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { axiosInstance } from "@/libs/axios";
+import { BASE_PATH } from "@/__generate__/artwork/base";
+import { useQuery } from "@tanstack/react-query";
+import Button from "@/components/ui/Button/Button/Button";
+import { useState, useEffect } from "react";
+
+const getMe = async () => {
+  const response = await axiosInstance({
+    url: `${BASE_PATH}/user/me`,
+  });
+  return response;
+};
+
+const useGetMe = () => {
+  return useQuery({
+    queryKey: ["me"],
+    queryFn: getMe,
+  });
+};
+
+export const Client = () => {
+  const { data, refetch } = useGetMe();
+  const [timestamp, setTimestamp] = useState<Date>();
+  const [cookie, setCookie] = useState("");
+
+  useEffect(() => {
+    setTimestamp(new Date());
+  }, [data]);
+
+  useEffect(() => {
+    const cookie = document.cookie
+      .split("; ")
+      .find((e) => e.startsWith("accessToken="))
+      ?.split("=")[1];
+    setCookie(cookie ?? "");
+  }, []);
+
+  return (
+    <>
+      <h2>client-side</h2>
+      <div>timestamp: {timestamp?.toISOString()}</div>
+      <div>cookie: {cookie}</div>
+      <pre>request headers:{JSON.stringify(data?.config.headers, null, 4)}</pre>
+      <pre>response data: {JSON.stringify(data?.data, null, 4)}</pre>
+      <Button type="primary" onClick={() => refetch()}>
+        Refetch
+      </Button>
+    </>
+  );
+};
--- libs/axios.ts
+++ libs/axios.ts
@@ -1,3 +1,16 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
+import { isBrowser } from "@/utils/browser";
 
 export const axiosInstance = axios.create();
+
+axiosInstance.interceptors.request.use((config: AxiosRequestConfig) => {
+  if (isBrowser()) {
+    const accessToken = document.cookie
+      .split("; ")
+      .find((e) => e.startsWith("accessToken="))
+      ?.split("=")[1];
+    config.headers = { ...config.headers, Authorization: accessToken };
+  } else {
+  }
+  return config;
+});
