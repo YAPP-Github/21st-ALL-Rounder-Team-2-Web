927bc460aa9d0fbcfe8222f6ae3d9907bc95382a
juhee <77ooee@gmail.com>
2023-04-19 14:59:29 +0900

    refactor: home layout 분리

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,5 @@
 import { Suspense } from "react";
-import { AppBar } from "@/components/pages/Home/AppBar/AppBar";
-import { CategoryList } from "@/components/pages/Home/CategoryList/CategoryList.server";
-import { SortDirectionSelect } from "@/components/pages/Home/SortDirectionSelect/SortDirectionSelect";
 import { ExhibitionCardList } from "@/components/pages/Home/ExhibitionCardList/ExhibitionCardList.server";
-import { PostFloatingButton } from "@/components/pages/Home/PostFloatingButton/PostFloatingButton";
-import styles from "./page.module.css";
 
 export default async function Page({
   searchParams,
@@ -15,23 +10,10 @@ export default async function Page({
   const category = searchParams?.category;
 
   return (
-    <>
-      <AppBar />
-      <div className={styles.category}>
-        <Suspense>
-          {/* @ts-expect-error Async Server Component */}
-          <CategoryList />
-        </Suspense>
-      </div>
-      <div className={styles.select}>
-        <SortDirectionSelect />
-      </div>
-      <Suspense>
-        {/* @ts-expect-error Async Server Component */}
-        <ExhibitionCardList direction={direction} category={category} />
-      </Suspense>
-      <PostFloatingButton />
-    </>
+    <Suspense>
+      {/* @ts-expect-error Async Server Component */}
+      <ExhibitionCardList direction={direction} category={category} />
+    </Suspense>
   );
 }
 
