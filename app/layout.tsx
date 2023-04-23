d78ccbb16a69fd1f1abf9a3847658b6a2da84cec
juhee <77ooee@gmail.com>
2023-04-23 21:23:45 +0900

    refactor: 다이나믹 서브셋 font 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,17 +13,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link
           rel="stylesheet"
           as="style"
-          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard.css"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard-dynamic-subset.css"
         />
       </head>
       <body>
         <AntdMobileProvider>
           <QueryClientWrapper>
             <StyledComponentsRegistry>
-              <>
-                <GlobalStyle />
-                {children}
-              </>
+              <GlobalStyle />
+              {children}
             </StyledComponentsRegistry>
             <ReactQueryDevtools initialIsOpen={false} />
           </QueryClientWrapper>
