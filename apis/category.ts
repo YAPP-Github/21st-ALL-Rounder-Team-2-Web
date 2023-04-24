fd0b4102586e27b022b75b30a6837aa351c75f9b
juhee <77ooee@gmail.com>
2023-04-24 21:49:22 +0900

    refactor: try~catch 제거

--- apis/category.ts
+++ apis/category.ts
@@ -4,10 +4,6 @@ import { CategoryControllerApiFactory } from "@/__generate__/category";
 const factory = CategoryControllerApiFactory(undefined, undefined, axiosInstance);
 
 export const getCategories = async () => {
-  try {
-    const response = await factory.getCategories();
-    return response.data;
-  } catch (err) {
-    return [];
-  }
+  const response = await factory.getCategories();
+  return response.data;
 };
