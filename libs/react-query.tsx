e3c8accf97d71e320a9e862e44f8591412b88016
juhee <77ooee@gmail.com>
2023-05-03 16:58:00 +0900

    feat: 화면이 refocus되면 쿼리 업데이트 수행

--- libs/react-query.tsx
+++ libs/react-query.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useVisibilityChange } from "@/hooks/useVisibilityChange";
 
 export default function QueryClientWrapper({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(
@@ -10,11 +11,18 @@ export default function QueryClientWrapper({ children }: { children: React.React
         defaultOptions: {
           queries: {
             retry: 0,
+            refetchOnWindowFocus: false,
             staleTime: 20 * 1000,
           },
         },
       })
   );
+
+  const invalidateAllQueries = () => {
+    queryClient.invalidateQueries();
+  };
+
+  useVisibilityChange({ onVisible: invalidateAllQueries });
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
