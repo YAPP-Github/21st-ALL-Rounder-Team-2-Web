48ab83b29cec166d7c4c595c7242f0075794755b
juhee <77ooee@gmail.com>
2023-05-09 13:45:53 +0900

    refactor: switch문 적용

--- components/Skeleton/Skeleton.styles.tsx
+++ components/Skeleton/Skeleton.styles.tsx
@@ -9,28 +9,32 @@ export const Skeleton = styled.div<Props>`
   border-radius: ${({ borderRadius }) => borderRadius};
   background: ${colors.overlay};
 
-  ${({ animation }) =>
-    animation === "wave" &&
-    css`
-      position: relative;
-      overflow: hidden;
+  ${({ animation }) => {
+    switch (animation) {
+      case "wave":
+        return css`
+          position: relative;
+          overflow: hidden;
 
-      &::after {
-        position: absolute;
-        content: "";
-        top: 0;
-        left: 0;
-        bottom: 0;
-        right: 0;
-        background: linear-gradient(90deg, transparent, ${colors.gray900} 60%, transparent);
-        opacity: 0.3;
-        transform: translateX(-100%);
-        animation: wave 1.6s linear 0.8s infinite;
-      }
-    `}
-  ${({ animation }) =>
-    animation === "purse" &&
-    css`
-      animation: pulse 1.5s ease-in-out 0.5s infinite;
-    `};
+          &::after {
+            position: absolute;
+            content: "";
+            top: 0;
+            left: 0;
+            bottom: 0;
+            right: 0;
+            background: linear-gradient(90deg, transparent, ${colors.gray900} 60%, transparent);
+            opacity: 0.3;
+            transform: translateX(-100%);
+            animation: wave 1.6s linear 0.8s infinite;
+          }
+        `;
+      case "purse":
+        return css`
+          animation: pulse 1.5s ease-in-out 0.5s infinite;
+        `;
+      default:
+        return;
+    }
+  }};
 `;
