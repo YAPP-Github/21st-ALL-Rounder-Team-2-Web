cc43178b3f1150323adff0e97114cd0ea467b5bc
juhee <77ooee@gmail.com>
2023-05-10 04:13:38 +0900

    refactor: page 구조 분리

--- app/exhibition/[exhibitionId]/page.tsx
+++ app/exhibition/[exhibitionId]/page.tsx
@@ -1,11 +1,12 @@
 import { Suspense } from "react";
 import { Hydrate } from "@tanstack/react-query";
 import { ErrorBoundary } from "@/components/ErrorBoundary/ErrorBoundary";
-import { ExhibitInformationHeader } from "./components/ExhibitInformationHeader/ExhibitInformationHeader";
+import { ExhibitInformationHeader as ExhibitInformationHeaderClient } from "./components/ExhibitInformationHeader/ExhibitInformationHeader";
 import { LinkPreviewCard, LinkPreviewCardError, LinkPreviewCardSkeleton } from "./components/LinkPreviewCard";
 import { ArtworkCardList, ArtworkCounter } from "./components/ArtworkCardList/ArtworkCardList.server";
 import { NavigationBar } from "./components/NavigationBar/NavigationBar";
 import { useFetchPostInfo } from "@/hooks/exhibition.server";
+import { useFetchArtworkList } from "@/hooks/artwork.server";
 import { getDehydratedState } from "@/libs/react-query-ssr/getDehydratedState";
 import * as S from "./page.styles";
 
@@ -16,7 +17,11 @@ export default async function PageWrapper({ params }: { params: { exhibitionId: 
       <NavigationBar exhibitionId={exhibitionId} />
       <Suspense>
         {/* @ts-expect-error Async Server Component */}
-        <Page exhibitionId={exhibitionId} />
+        <ExhibitInformationHeader exhibitionId={exhibitionId} />
+      </Suspense>
+      <Suspense>
+        {/* @ts-expect-error Async Server Component */}
+        <PageContent exhibitionId={exhibitionId} />
       </Suspense>
     </>
   );
@@ -26,14 +31,25 @@ type Props = {
   exhibitionId: number;
 };
 
-async function Page({ exhibitionId }: Props) {
-  const postInfo = await useFetchPostInfo(exhibitionId);
+async function ExhibitInformationHeader({ exhibitionId }: Props) {
+  await useFetchPostInfo(exhibitionId);
   const dehydratedState = getDehydratedState();
 
   return (
     <Hydrate state={dehydratedState}>
-      <NavigationBar exhibitionId={exhibitionId} />
-      <ExhibitInformationHeader exhibitionId={exhibitionId} />
+      <ExhibitInformationHeaderClient exhibitionId={exhibitionId} />
+    </Hydrate>
+  );
+}
+
+async function PageContent({ exhibitionId }: Props) {
+  const postInfoData = useFetchPostInfo(exhibitionId);
+  const artworkListData = useFetchArtworkList(exhibitionId);
+  const [postInfo] = await Promise.all([postInfoData, artworkListData]);
+  const dehydratedState = getDehydratedState();
+
+  return (
+    <Hydrate state={dehydratedState}>
       <S.Content>
         <Suspense>
           {/* @ts-expect-error Async Server Component */}
