b0d102ebbc6a7446bf59bd309cfe95bd609eaf08
juhee <77ooee@gmail.com>
2023-05-25 11:14:02 +0900

    feat: 이미지 로딩 실패시 errorFallback 처리

--- components/Image/Image.styles.tsx
+++ components/Image/Image.styles.tsx
@@ -4,3 +4,12 @@ export const Fade = styled.div<{ isLoaded?: boolean }>`
   opacity: ${({ isLoaded }) => (isLoaded ? 1 : 0)};
   transition: opacity 0.3s;
 `;
+
+export const ErrorFallbackWrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(255, 255, 255, 0.2);
+`;
