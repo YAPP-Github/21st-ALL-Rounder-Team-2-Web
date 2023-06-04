1fd58c5eaf13203934621f8435d98438d8db7d21
juhee <77ooee@gmail.com>
2023-06-04 19:54:33 +0900

    refactor: header 로직 삭제

--- components/AuthProvider/AuthProvider.tsx
+++ components/AuthProvider/AuthProvider.tsx
@@ -1,11 +1,10 @@
-import { headers, cookies } from "next/headers";
+import { cookies } from "next/headers";
 import { PropsWithChildren } from "react";
 import { axiosInstance } from "@/libs/axios";
 
 export const AuthProvider = ({ children }: PropsWithChildren) => {
-  const headerInstance = headers();
   const cookieStore = cookies();
-  const accessToken = headerInstance.get("authorization") ?? cookieStore.get("accessToken")?.value;
+  const accessToken = cookieStore.get("accessToken")?.value;
   axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
 
   if (!accessToken) {
