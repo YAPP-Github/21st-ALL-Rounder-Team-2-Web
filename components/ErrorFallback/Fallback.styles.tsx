137fa7ea4853cff41076a2278b7a6fde39bb8d59
juhee <77ooee@gmail.com>
2023-06-12 00:02:36 +0900

    style: error fallback 쌓임 맥락 추가

--- components/ErrorFallback/Fallback.styles.tsx
+++ components/ErrorFallback/Fallback.styles.tsx
@@ -5,13 +5,17 @@ import { Medium16CSS, Normal16CSS } from "@/components/Typographies";
 import { colors } from "@/styles/colors";
 
 export const Wrapper = styled.div`
+  position: absolute;
+  z-index: 1100;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   gap: 20px;
-  width: 100%;
-  height: 100vh;
 `;
 
 export const Description = styled.div`
